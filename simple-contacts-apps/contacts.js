c987e7137a46b3685e92913bee0807a0b73df3a2
Zalbaq <ihza.nk.160903@gmail.com>
2023-10-15 23:26:59 +0700

    [add] listData function to show all json data

---
+++ simple-contacts-apps/contacts.js
@@ -0,0 +1,72 @@
+import fs from "fs";
+import ReadLine from "readline";
+import { stdin as input, stdout as output } from "process";
+import { addAbortSignal } from "stream";
+
+const rl = ReadLine.createInterface({ input, output });
+const question = (quest) => {
+  return new Promise((resolve) => {
+    rl.question(`${quest}`, (ans) => {
+      resolve(ans);
+    });
+  });
+};
+
+const checkDir = (dirPath) => {
+  new Promise((resolve) => {
+    if (!fs.existsSync(dirPath)) resolve(fs.mkdirSync(dirPath));
+  });
+};
+
+const checkFile = (filePath) => {
+  new Promise((resolve) => {
+    if (!fs.existsSync(filePath))
+      resolve(fs.writeFileSync(filePath, "[]", "utf-8"));
+  });
+};
+
+const checkPath = () => {
+  const dirPath = "data";
+  const filePath = "data/contacts.json";
+
+  checkDir(dirPath);
+  checkFile(filePath);
+};
+
+const loadContact = () => {
+  const dataBuffer = fs.readFileSync("./data/contacts.json", "utf-8");
+  const contactsJSON = JSON.parse(dataBuffer);
+  return contactsJSON;
+};
+const pushData = (data) => {
+  const contactsJSON = loadContact();
+  const isDuplicate = contactsJSON.find(
+    (contact) => contact.name === data.name
+  );
+
+  if (isDuplicate) {
+    console.log("Nama sudah terdaftar!!");
+    rl.close();
+  } else {
+    contactsJSON.push(data);
+    fs.writeFileSync(
+      "data/contacts.json",
+      JSON.stringify(contactsJSON),
+      "utf-8"
+    );
+    console.log("Terimakasih telah memasukkan data anda");
+  }
+  rl.close();
+};
+const listData = () => {
+  const contacts = loadContact();
+  contacts.forEach((contact, i) => {
+    console.log(
+      `${i + 1}. ${contact.name} - ${contact.age} - ${contact.noHp} - ${
+        contact.email
+      }`
+    );
+  });
+};
+
+export { pushData, question, checkPath, listData };
