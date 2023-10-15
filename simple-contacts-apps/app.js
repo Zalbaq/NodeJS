b82933db12cf3d9643d7b073195f801dee25d8c0
Zalbaq <ihza.nk.160903@gmail.com>
2023-10-15 23:32:43 +0700

    [bug] when typing, double letters will appear

--- simple-contacts-apps/app.js
+++ simple-contacts-apps/app.js
@@ -1,24 +1,15 @@
 import yargs from "yargs";
-import { addData, question, checkPath } from "./local_modules/contacts.js";
 import { hideBin } from "yargs/helpers";
-import { oneLine } from "./local_modules/oneLineInterface.js";
+import { checkPath } from "./contacts.js";
+import { setupOneLine } from "./oneLineInterface.js";
+import { setupMultiLine } from "./multiLineInterface.js";
 
-const argv = Object.keys(yargs(hideBin(process.argv)).argv); // Menjadikan objek agar bisa melakukan destructuring
-const lengthArgv = [...argv].length; // Destructuring objek menjadi array dan mengambil length nya
+const argv = yargs(hideBin(process.argv)).argv;
+const { setupComand } = setupOneLine;
 
 const main = async () => {
-  if (lengthArgv <= 2) {
-    const data = {};
-    checkPath();
-    data.nama = await question("Masukkan nama anda: ");
-    data.umur = await question("Masukkan umur anda: ");
-    data.noHp = await question("Masukkan no HP anda: ");
-    data.email = await question("Masukkan email anda: ");
-
-    return addData(data) ? true : false;
-  } else {
-    oneLine();
-  }
-  console.log("Terimakasih sudah memasukkan data anda");
+  checkPath();
+  argv._.length === 0 ? setupMultiLine() : setupComand();
 };
+
 main();
