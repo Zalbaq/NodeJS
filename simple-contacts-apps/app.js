9b3fe3d7a0057cb6cd9fe1a3343c76157011b63b
Zalbaq <ihza.nk.160903@gmail.com>
2023-10-17 20:46:19 +0700

    [Remove] destructuring on setupOneLine

--- simple-contacts-apps/app.js
+++ simple-contacts-apps/app.js
@@ -1,15 +1,14 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { checkPath } from "./contacts.js";
-import { setupOneLine } from "./oneLineInterface.js";
+import { setupComand } from "./oneLineInterface.js";
 import { setupMultiLine } from "./multiLineInterface.js";
 
 const argv = yargs(hideBin(process.argv)).argv;
-const { setupComand } = setupOneLine;
 
-const main = async () => {
+const main = () => {
   checkPath();
   argv._.length === 0 ? setupMultiLine() : setupComand();
 };
 
-main();
+main()
